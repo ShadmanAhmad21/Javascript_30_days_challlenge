85fca42061040085f900c3b9bb571190a0b5c3cd
ShadmanAhmad21 <s.shadmanahmad2138@gmail.com>
2024-07-16 21:14:41 +0530

    Day 2 challenge

---
+++ JavaScript_30_days_challenge/Day_02.js
@@ -0,0 +1,52 @@
+// Task 1
+let a = 5
+let b = 2
+console.log("Sum : ",a+b)
+// Task 2
+console.log("Difference : ",a-b)
+
+// Task 3
+console.log("Product : ",a*b)
+
+// Task 4
+console.log("Quotient : ",a/b)
+
+// Task 5
+console.log("Remainder : ",a%b)
+
+// Task 6
+a+=b
+console.log("a+=b : ",a)
+
+// Task 7
+a-=b
+console.log("a-=b : ",a)
+
+let c = 10
+let d = "10"
+// Task 8
+console.log("> : ",a>b)
+console.log("< : ",a<b)
+
+// Task 9
+console.log(">= : ",a>=b)
+console.log("<= : ",a<=b)
+
+// Task 10
+console.log("== : ",c==d)
+console.log("=== : ",c===d)
+
+let age = 17
+// Task 11
+console.log("&& : ", age>18 && age<67)
+
+// Task 12
+console.log("|| : ", age>18 || age<67)
+
+// Task 13
+console.log("! : ", !(age>18))
+
+// Task 14
+let n = 7
+let ans = (n>0 ? "+ive" : n<0 ? "-ive" : "zero")
+console.log("n : ",ans)
