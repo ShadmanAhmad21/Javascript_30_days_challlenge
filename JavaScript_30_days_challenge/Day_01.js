0786a1054f32c2cb1c189105d7b68ebb57c1f868
ShadmanAhmad21 <s.shadmanahmad2138@gmail.com>
2024-07-16 20:49:21 +0530

    first commit

---
+++ JavaScript_30_days_challenge/Day_01.js
@@ -0,0 +1,52 @@
+// Task 1
+
+var number = 12;
+console.log(number);
+
+
+// Task 2
+
+let string = "This is a string";
+console.log(string);
+
+
+// Task 3
+
+const bool = true ;
+console.log(bool);
+
+
+// Task 4
+
+let num = 21;
+console.log(typeof(num))
+
+let str = "String";
+console.log(typeof(str))
+
+let boolean = false;
+console.log(typeof(boolean))
+
+let array = new Array (1,2,3,4,5);
+console.log(typeof(array))
+
+let obj = {name:"shadman",
+age:16};
+console.log(typeof(obj))
+
+
+// Task 5
+
+let myvar
+console.log("Initial : ",myvar)
+myvar = 7
+console.log("Final : ",myvar)
+
+
+
+// Task6
+
+const notvar = 45
+console.log(notvar)
+// notvar = 7 // const can't be changed or reassinged to new value
+console.log(notvar)
